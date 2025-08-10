cdaf4070af2d21d38a3d55c814d7a2bf5b0a2aa4
damduy <80811184+damduy@users.noreply.github.com>
2025-08-10 13:38:43 +0700

    JS have dynamic var type while TS have more narrow var type

---
+++ src/chapter10/video87/video87.js
@@ -0,0 +1,9 @@
+console.log("video 87 with js")
+
+let age = 25; //dynamic type
+
+age = "damduy";
+
+age = [1, 2, 3];
+
+console.log(age)
---
+++ src/chapter10/video87/video87.ts
@@ -0,0 +1,11 @@
+console.log("Video 87 with typescript")
+
+
+let age = 25;
+
+//Error
+// age = "damduy";
+
+// age = [1, 2, 3];
+
+// console.log(age)
