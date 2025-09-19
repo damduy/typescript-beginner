73b9f74927e41366fe30c19961f8f9b517ecf48a
damduy <80811184+damduy@users.noreply.github.com>
2025-09-19 17:30:32 +0700

    Annotation and Type infer

---
+++ src/chapter10/video91/video91.ts
@@ -0,0 +1,20 @@
+export{};
+
+console.log("Video 91");
+
+//annotation : @Controller @Service //comment fore machine to understand
+
+//infer
+
+
+const name: string = "duy";
+const age: number = 25;
+const test: boolean = true;
+
+let username = "asdasdas";
+let number = 12;
+
+const score: number[] = [1, 2, 3]; //Array<number>
+
+const demo: any = "Any"; //han che dung any
+const demo1: unknown = "Unknown";
