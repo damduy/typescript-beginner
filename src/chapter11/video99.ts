ba5b8dc7058df1bc4cfe8b74ab2ae0a03609609c
damduy <80811184+damduy@users.noreply.github.com>
2025-09-29 15:18:38 +0700

    Null and Undefined

---
+++ src/chapter11/video99.ts
@@ -0,0 +1,20 @@
+export {};
+
+console.log("Video 99")
+
+type TName = string | null;
+
+let name: TName = null;
+
+name = "Duy";
+
+const age = undefined;
+
+const printInfo = (myName: null | string) => {
+    // if(myName)
+    // myName.toUpperCase();
+
+    (myName!).toUpperCase();
+
+    console.log("name = ", myName);
+}
