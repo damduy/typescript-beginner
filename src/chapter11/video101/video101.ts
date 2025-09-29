eff9e9574fa48a650553440fc8991ec9e0d35e4a
damduy <80811184+damduy@users.noreply.github.com>
2025-09-29 15:58:36 +0700

    Tuple

---
+++ src/chapter11/video101/video101.ts
@@ -0,0 +1,12 @@
+export{ };
+console.log("Video 101");
+
+type TTuple = [string, number, boolean?]; //3
+
+const test = ["damduy", 25, true]
+
+const test1 : TTuple = ["duy", 100, true]
+
+const test2 : [string, number, boolean] = ["duy", 100, true]
+
+const test3 : TTuple = ["duy", 100]
